ea907bdebe885ce071c010aa81f08eb3a6e00e26
Becky Skillings <beckyskillings@gmail.com>
2015-10-06 10:23:16 -0600

    get and post functional and tidy

--- simple_server.rb
+++ simple_server.rb
@@ -1,56 +1,64 @@
-require "socket"#get sockets from stdlib
-require "json"
-require "erb"
+class SimpleServer
 
-server = TCPServer.open(2000) #socket to listen on port 2000
-loop {  #servers run forever
-	client = server.accept #wait for a client to connect
-	request = client.recv(1024) #only the first line is coming through
-	puts request
-	request_split = request.split(" ")
-	if (request_split[0] == "GET") #working
-		response = ""
-		filename = request_split[1]
+	require "socket"#get sockets from stdlib
+	require "json"
+
+	def initialize
+
+		server = TCPServer.open(2000) #socket to listen on port 2000
+		loop {  #servers run forever
+			@client = server.accept #wait for a client to connect
+			@request = @client.recv(2048) #only the first line is coming through
+			puts @request
+			@request_split = @request.split(" ")
+			if (@request_split[0] == "GET") then respond_to_get
+			elsif (@request_split[0] == "POST") then respond_to_post
+			else
+				@client.print "HTTP/1.0 404 File Not Found (bad request)"
+			end
+			@client.puts ""
+			@client.puts (Time.now.ctime) #send the time to the client 
+			@client.puts "Closing the connection.  Bye!"
+			@client.close #disconnect from the client
+		}
+	end
+
+	def respond_to_get
+		filename = @request_split[1]
 		filename.slice!(0)#taking / off front
 		puts filename
 		if File.file?(filename)
 			file_text = File.open(filename, "r").read
-			puts "File: #{file_text}"
-			client.print "HTTP/1.0 200 OK\r\n"
-			client.print "Date : #{Time.now.ctime}\r\n"
-			client.print "Content-Length: #{file_text.length}\r\n"
-			client.print "\r\n"
-			client.print file_text
+			puts "File: #{file_text}" #building the response
+			@client.print "HTTP/1.0 200 OK\r\n"
+			@client.print "Date : #{Time.now.ctime}\r\n"
+			@client.print "Content-Length: #{file_text.length}\r\n"
+			@client.print "\r\n"
+			@client.print file_text
 		else
-			client.print "HTTP/1.0 404 File Not Found"
+			@client.print "HTTP/1.0 404 File Not Found"
 		end
-	elsif (request_split[0] == "POST")
+			
+	end
+
+	def respond_to_post
 		puts "got a post request"
-		puts request
 		#split response at first blank line into headers and body
-		headers, body = request.split("\r\n\r\n", 2)
+		headers, body = @request.split("\r\n\r\n", 2)
 		header_lines = headers.split("\r\n")
 		params = JSON.parse(body)
 		vikings =  params["viking"]
 		viking_elements = ""
 		vikings.each {|k, v| viking_elements += "<li> #{k}: #{v}</li>"}
 		thanks_page = File.read("thanks.html")
-		this_thanks = thanks_page.gsub("<%= yield %>", viking_elements)
-		send_me = "HTTP/1.0 200 OK\r\nDate : #{Time.now.ctime}\r\n\r\n#{this_thanks}"
-		puts send_me
-		client.print send_me
-		#	client.print "Date : #{Time.now.ctime}\r\n"
-	#	client.print "Content-Length: #{file_text.length}\r\n"
-	#	client.print "\r\n"
-	#	client.print this_thanks
-		#this is barfing again
-	else
-		client.print "HTTP/1.0 404 File Not Found (bad request)"
+		this_thanks = thanks_page.gsub("<%= yield %>", viking_elements) #swapping text for yield
+		@client.print "HTTP/1.0 200 OK \r\n" #building the response
+		@client.print "Date : #{Time.now.ctime}\r\n"
+		@client.print "Content-Length: #this_thanks.length}\r\n"
+		@client.print "\r\n"
+		@client.print this_thanks
 	end
-#	client.puts ""
-#	client.puts (Time.now.ctime) #send the time to the client why isn't it doing this anymore?
-#	client.puts "Closing the connection.  Bye!"
-	client.close #disconnect from the client
-}
 
-	
+end
+
+server = SimpleServer.new
--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -1,60 +1,71 @@
+class TinyBrowser
+
 	require "socket"
 	require "json"
 
-	host = "localhost" #the web server
-	port = 2000 #default http port
+	def initialize
 
-	puts "Do you want to get something or post something?"
-	user_action = gets.chomp.downcase
-	if user_action ==  "get" #this works as far as I can tell
+		@host = "localhost" #the web server
+		@port = 2000 #default http port
+
+		puts "Do you want to get something or post something?"
+		user_action = gets.chomp.downcase
+		if user_action ==  "get" then @response = get_something
+		elsif user_action == "post" then @response = post_something
+		else 
+			puts "I can only get or post.  Goodbye!"
+		end
+	end
+
+	def get_something
 		puts "Enter filename like this: /???.???"
 		path = gets.chomp.downcase #the file we want
-			#this is the HTTP request we send to fetch a file
-		request = "GET #{path} HTTP/1.0\r\n\r\n"
+		
+		@request = "GET #{path} HTTP/1.0\r\n\r\n" #get request is simple
 
-		socket = TCPSocket.open(host, port) #connect to server
-		socket.print(request) #send request
-		response = socket.read #read complete response
-		#split response at first blank line into headers and body
-		headers, body = response.split("\r\n\r\n", 2)
-		header_lines = headers.split("\r\n")
-		if /1\.0\s4../.match(header_lines[0])
-			puts "#{header_lines[0]}"
-		else
-			print body #and display it
-		end
-	elsif user_action == "post"
-	 	puts "Welcome to viking registration.  What is the name of your viking?"
+		talk_to_socket
+
+		print_response
+	end
+
+	def post_something
+		puts "Welcome to viking registration.  What is the name of your viking?"
 		name = gets.chomp.capitalize
 		puts "What is your viking's email?"
 		email = gets.chomp.downcase
 		new_viking_info = {:name => name, :email => email}
 		new_viking = {:viking => new_viking_info}
 		new_viking = new_viking.to_json
-		socket = TCPSocket.open(host, port)
-		request = "POST localhost HTTP/1.0\r\n"
-		request += "Date: #{Time.now.ctime}\r\n"
-		request += "Content-Type: text/html\r\n"
-		request += "Content-Length: #{new_viking.length}\r\n"
-		request += "Content-Length: #{4}\r\n"
-		request += "\r\n"
-		request += new_viking
-		socket.print(request)
-		response = socket.recv(1024) #read complete response
-		puts response
-		#split response at first blank line into headers and body
-#		headers, body = response.split("\r\n\r\n", 2)
-#		header_lines = headers.split("\r\n")
-#		if /1\.0\s4../.match(header_lines[0])
-#			puts "#{header_lines[0]}"
-		#	print body #and display it
-		#end
+		
+		@request = "POST localhost HTTP/1.0\r\n"#building request into one string
+		@request += "Date: #{Time.now.ctime}\r\n"
+		@request += "Content-Type: text/html\r\n"
+		@request += "Content-Length: #{new_viking.length}\r\n"
+		@request += "\r\n"
+		@request += new_viking
+		
+		talk_to_socket
 
-		#it's trying to read the response that's causing it to barf on both parts now
-	
-	else 
-		puts "I can only get or post.  Goodbye!"
+		print_response
+	end
+
+	def talk_to_socket
+		@socket = TCPSocket.open(@host, @port)
+		@socket.print(@request)
+
+	end
+
+	def print_response
+		@response = @socket.recv(1024) #read complete response
+		#split response at first blank line into headers and body
+		headers, body = @response.split("\r\n\r\n", 2)
+		header_lines = headers.split("\r\n")
+		if /1\.0\s4../.match(header_lines[0])
+			puts "#{header_lines[0]}"
+		else
+			print body #and display it
+		end	
+	end
 end
 
-#having trouble separating this into methods, which I feel pretty stupid about
-#also not sure how to set up post. not sure what goes in the middle of the top line
+browser = TinyBrowser.new
